5ed65cbe21d3e6830a91564598a80b2b6d783411
psh0121 <tngus2057@gmail.com>
2023-03-26 04:28:38 +0900

    [Day01] 두 수의 합

---
+++ Day01/1-1.js
@@ -0,0 +1,11 @@
+// 두 수의 합
+
+// 정수 num1과 num2가 주어질 때, num1과 num2의 합을 
+// return하도록 solution함수를 완성해주세요.
+
+function solution(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(solution(2, 3));    // 5
+console.log(solution(100, 2));    // 102
