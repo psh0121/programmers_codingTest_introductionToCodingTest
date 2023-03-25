8b0954aeb1fe368046d43c2056872ee4f76a43b1
psh0121 <tngus2057@gmail.com>
2023-03-26 04:31:21 +0900

    [Day01] 두 수의 차

---
+++ Day01/1-2.js
@@ -0,0 +1,11 @@
+// 두 수의 차
+
+// 정수 num1과 num2가 주어질 때, num1과 num2 를 뺀 값을
+// return하도록 solution함수를 완성해주세요.
+
+function solution(num1, num2) {
+    return num1 - num2;
+}
+
+console.log(solution(2, 3));    // -1
+console.log(solution(100, 2));    // 98
