7b9174818991934e4cd36c6068d7b310b3d7556f
psh0121 <tngus2057@gmail.com>
2023-04-05 05:32:57 +0900

    [Day06] 문자 반복 출력하기

---
+++ Day06/6-4.js
@@ -0,0 +1,18 @@
+// 문자 반복 출력하기
+
+// 문자열 my_string과 정수 n이 매개변수로 주어질 때, 
+// my_string에 들어있는 각 문자를 n만큼 반복한 문자열을 
+// return 하도록 solution 함수를 완성해보세요.
+
+function solution(my_string, n) {
+    let arr = my_string.split("");
+    let result = "";
+    
+    for(let i = 0; i < arr.length; i++){
+        result += arr[i].repeat(n);
+    }
+    
+    return result;
+}
+
+console.log(solution("hello")); // "hhheeellllllooo";
