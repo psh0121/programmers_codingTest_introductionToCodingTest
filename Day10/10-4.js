c078a29d5fa1064d2b2b525f423fe8460268b93b
psh0121 <tngus2057@gmail.com>
2023-04-10 16:06:21 +0900

    [Day10] 배열 회전시키기

---
+++ Day10/10-4.js
@@ -0,0 +1,18 @@
+// 배열 회전시키기
+
+// 정수가 담긴 배열 numbers와 문자열 direction가 매개변수로 주어집니다. 
+// 배열 numbers의 원소를 direction방향으로 한 칸씩 회전시킨 배열을 return하도록 solution 함수를 완성해주세요.
+
+function solution(numbers, direction) {
+    if(direction === "right"){
+        numbers.unshift(numbers.pop());
+    }
+    else {
+        numbers.push(numbers.shift());
+    }
+    
+    return numbers;
+}
+
+console.log(solution([1, 2, 3], "right"));  // [3, 1, 2]
+console.log(solution([4, 455, 6, 4, -1, 45, 6], "left"));   // [455, 6, 4, -1, 45, 6, 4]
